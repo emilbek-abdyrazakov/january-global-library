1d474f3e43f6ae1b15714c1916d57ad634ce2f95
Emil Razak <moorkgz@gmail.com>
2022-06-17 18:48:57 +0000

    new text

--- src/com/lib/test.groovy
+++ src/com/lib/test.groovy
@@ -1,7 +1,21 @@
-def say_hello() {
-    node {
-        stage("Stage1"){
-            sh "echo Hello world!"
+pipeline {
+    agent any
+
+    stages {
+        stage('Build') {
+            steps {
+                echo 'Building..'
+            }
+        }
+        stage('Test') {
+            steps {
+                echo 'Testing..'
+            }
+        }
+        stage('Deploy') {
+            steps {
+                echo 'Deploying....'
+            }
         }
     }
 }
